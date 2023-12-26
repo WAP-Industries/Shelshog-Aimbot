9e1cc6d9de4aac18c0e050645470a8f281eb5dc8
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2023-12-26 20:08:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 // ==UserScript==
-// @name         Shellshock.IO Aimbot & ESP
+// @name         Shelshog Aimbot
+// @author       WAP Industries
 // @namespace    http://tampermonkey.net/
-// @version      0.2.2
-// @description  Locks aim to the nearest player in shellshock.io. Comes with an ESP too. Press B, V, N, L to toggle aimbot, esp, esp lines, aimbot on right mouse hold.
-// @author       Zertalious (Zert)
 // @match        *://shellshock.io/*
 // @match        *://algebra.best/*
 // @match        *://algebra.vip/*
@@ -50,127 +48,91 @@
 // @icon         https://www.google.com/s2/favicons?domain=shellshock.io
 // @grant        none
 // @run-at       document-start
-// @antifeature  ads
-// @downloadURL https://update.greasyfork.org/scripts/436330/ShellshockIO%20Aimbot%20%20ESP.user.js
-// @updateURL https://update.greasyfork.org/scripts/436330/ShellshockIO%20Aimbot%20%20ESP.meta.js
 // ==/UserScript==
 
 window.XMLHttpRequest = class extends window.XMLHttpRequest {
+    open(_, url) {
+        if (url.indexOf('shellshock.js') > - 1) 
+            this.isScript = true;
+        return super.open(...arguments);
+    }
 
-	open( method, url ) {
+    get response(){
+        if (this.isScript){
+            const code = super.response
 
-		if ( url.indexOf( 'shellshock.js' ) > - 1 ) {
+            const variables = {
+                babylon: /this\.origin=new ([a-zA-Z]+)\.Vector3/.exec(code)?.[1],
+                players: /([^,]+)=\[\],[^,]+=\[\],[^,]+=-1,vueApp.game.respawnTime=0/.exec(code)?.[1],
+                myPlayer: /"fire":document.pointerLockElement&&([^&]+)&&/.exec(code)?.[1],
+                scene: /createMapCells\(([^,]+),/.exec(code)?.[1],
+                cullFunc: /=([a-zA-Z_$]+)\(this\.mesh,\.[0-9]+\)/.exec(code)?.[1],
+                game: /([^,]+).playerAccount=/.exec(code)?.[1]
+            }
 
-			this.isScript = true;
+            if (Object.values(variables).some(i=>!i))
+                return void alert(`Script failed to inject\n\nVariables missing:\n${Object.keys(variables).filter(i=>!variables[i]).join('\n')}`)
 
-		}
+            console.log('%cScript injected', 'color: red; background: black; font-size: 2em;', variables);
 
-		return super.open( ...arguments );
+            return code.replace(variables.scene + '.render()', `
+                    window['${onUpdateFuncName}'](${variables.babylon},${variables.players},${variables.myPlayer}); 
+                    ${variables.scene}.render()`)
+                .replace(`function ${variables.cullFunc}`, `
+                    function ${variables.cullFunc}() {return true;}
+                    function someFunctionWhichWillNeverBeUsedNow`)
+                .replace(
+                    `for(var i=0;i<this.inventory.length;i++)`,
+                    `return !0;`
+                )
+                .replace(
+                    `console.log("startGame()");`,
+                    `
+                        console.log("startGame()"); 
+                        window['${disguiseFunc}'](${variables.game});
+						vueApp.$refs.equipScreen.$refs.weapon_select.selectClass(
+							CharClass[${variables.game}.playerAccount.getPrimaryWeapon().category_name.replace(" Primary Weapons", "")]
+						);
+                    `,
+                )
+        }
 
-	}
-
-	get response() {
-
-		if ( this.isScript ) {
-
-			const code = super.response;
-
-			let babylonVarName,
-				playersVarName,
-				myPlayerVarName,
-				sceneVarName,
-				cullFuncName;
-
-			try {
-
-				babylonVarName = /this\.origin=new ([a-zA-Z]+)\.Vector3/.exec( code )[ 1 ];
-				playersVarName = /=([a-zA-Z]+)\[this\.playerIdx\];/.exec( code )[ 1 ];
-				myPlayerVarName = /"fire":document.pointerLockElement&&([^&]+)&&/.exec( code )[ 1 ];
-				sceneVarName = /createMapCells\(([^,]+),/.exec( code )[ 1 ];
-				cullFuncName = /=([a-zA-Z_$]+)\(this\.mesh,\.[0-9]+\)/.exec( code )[ 1 ];
-
-			} catch ( error ) {
-
-				alert( 'Script failed to inject. Report the issue to the script developer.\n' + JSON.stringify( getVars(), undefined, 2 ) );
-
-				return code;
-
-			}
-
-			function getVars() {
-
-				return {
-					babylonVarName,
-					playersVarName,
-					myPlayerVarName,
-					playersVarName,
-					sceneVarName,
-					cullFuncName
-				};
-
-			}
-
-			console.log( '%cInjecting code...', 'color: red; background: black; font-size: 2em;', getVars() );
-
-			return code.replace( sceneVarName + '.render()', `
-
-					window[ '${onUpdateFuncName}' ](
-						${babylonVarName},
-						${playersVarName},
-						${myPlayerVarName}
-					);
-
-				${sceneVarName}.render()` )
-				.replace( `function ${cullFuncName}`, `
-
-					function ${cullFuncName}() {
-
-						return true;
-
-					}
-
-				function someFunctionWhichWillNeverBeUsedNow` );
-
-		}
-
-		return super.response;
-
-	}
-
-};
-
-let espEnabled = true;
-let aimbotEnabled = true;
-let showLines = true;
-let aimbotOnRightMouse = true;
-
-let rightMouseDown = false;
-
-function handleMouse( event ) {
-	rightMouseDown = event.type === 'pointerdown';
+        return super.response
+    }
 }
 
-window.addEventListener( 'pointerdown', handleMouse );
-window.addEventListener( 'pointerup', handleMouse );
+
+let rightMouseDown = false,
+lineOrigin, lines;
 
 
-let lineOrigin, lines;
+const genName = ()=> btoa(Math.random().toString(32)),
+    onUpdateFuncName = genName(),
+    disguiseFunc = genName()
 
-const onUpdateFuncName = btoa( Math.random().toString( 32 ) );
+window[disguiseFunc] = function(game){
+    for (const i of ["hats", "grenades", "melee", "primaryWeapons", "secondaryWeapons"]){
+        const items = extern.catalog[i].filter(i=>
+            i=="primaryWeapons" ? 
+            i.category_name.includes(game.playerAccount.getPrimaryWeapon().category_name.replace(" Primary Weapons", "")):
+            !0
+        )
+        extern.tryEquipItem(items[Math.floor(Math.random()*items.length)])
+    }
+}
+
+window.onmousemove = ()=>{
+	try{
+		if (!vueApp?.game.on) PlayerNameInput.methods.onNameChange({target: {value: (Math.random()+1).toString(36).substring(2)}})
+	}catch{}
+}
 
 window[ onUpdateFuncName ] = function ( BABYLON, players, myPlayer ) {
-
-	if ( ! myPlayer ) {
-
-		return;
-
-	}
+	if ( ! myPlayer ) return;
 
 	if ( ! lineOrigin ) {
-
 		lineOrigin = new BABYLON.Vector3();
 		linesArray = [];
-
 	}
 
 	lineOrigin.copyFrom( myPlayer.actor.mesh.position );
@@ -181,24 +143,15 @@ window[ onUpdateFuncName ] = function ( BABYLON, players, myPlayer ) {
 	lineOrigin.z += Math.cos( yaw );
 	lineOrigin.y += Math.sin( - myPlayer.pitch );
 
-	for ( let i = 0; i < linesArray.length; i ++ ) {
-
+	for ( let i = 0; i < linesArray.length; i ++ )
 		linesArray[ i ].playerExists = false;
 
-	}
-
 	for ( let i = 0; i < players.length; i ++ ) {
-
 		const player = players[ i ];
 
-		if ( ! player || player === myPlayer ) {
-
-			continue;
-
-		}
+		if ( ! player || player === myPlayer ) continue;
 
 		if ( player.sphere === undefined ) {
-
 			console.log( 'Adding sphere...' );
 
 			const material = new BABYLON.StandardMaterial( 'myMaterial', player.actor.scene );
@@ -212,11 +165,9 @@ window[ onUpdateFuncName ] = function ( BABYLON, players, myPlayer ) {
 			sphere.parent = player.actor.mesh;
 
 			player.sphere = sphere;
-
 		}
 
 		if ( player.lines === undefined ) {
-
 			const options = {
 				points: [ lineOrigin, player.actor.mesh.position ],
 				updatable: true
@@ -233,68 +184,56 @@ window[ onUpdateFuncName ] = function ( BABYLON, players, myPlayer ) {
 			linesArray.push( lines );
 
 			console.log( '%cAdding line...', 'color: green; background: black; font-size: 2em;' );
-
 		}
 
 		player.lines.playerExists = true;
 		player.lines = BABYLON.MeshBuilder.CreateLines( 'lines', player.lineOptions );
 
-		player.sphere.renderingGroupId = espEnabled ? 1 : 0;
-		player.sphere.visibility = ( aimbotEnabled || espEnabled ) && myPlayer !== player && ( myPlayer.team === 0 || myPlayer.team !== player.team );
+		player.sphere.renderingGroupId = 1
+		player.sphere.visibility = myPlayer !== player && ( myPlayer.team === 0 || myPlayer.team !== player.team );
 
-		player.lines.visibility = player.playing && player.sphere.visibility && showLines;
+		player.lines.visibility = player.playing && player.sphere.visibility;
 
 	}
 
 	for ( let i = 0; i < linesArray.length; i ++ ) {
-
 		if ( ! linesArray[ i ].playerExists ) {
-
 			console.log( '%cRemoving line...', 'color: red; background: black; font-size: 2em;' );
-
 			linesArray[ i ].dispose();
 			linesArray.splice( i, 1 );
-
 		}
-
 	}
 
-	if ( aimbotEnabled && ( aimbotOnRightMouse ? rightMouseDown : true ) && myPlayer.playing ) {
-
+	if (rightMouseDown && myPlayer.playing ) {
 		let minDistance = Infinity;
 		let targetPlayer;
 
 		for ( let i = 0; i < players.length; i ++ ) {
-
 			const player = players[ i ];
 
 			if ( player && player !== myPlayer && player.playing && ( myPlayer.team === 0 || player.team !== myPlayer.team ) ) {
-
 				const distance = Math.hypot( player.x - myPlayer.x, player.y - myPlayer.y, player.z - myPlayer.z );
-
 				if ( distance < minDistance ) {
-
 					minDistance = distance;
-
 					targetPlayer = player;
-
 				}
-
 			}
-
 		}
 
 		if ( targetPlayer ) {
-
 			const x = targetPlayer.actor.mesh.position.x - myPlayer.actor.mesh.position.x;
 			const y = targetPlayer.actor.mesh.position.y - myPlayer.actor.mesh.position.y;
 			const z = targetPlayer.actor.mesh.position.z - myPlayer.actor.mesh.position.z;
 
 			myPlayer.yaw = Math.radAdd( Math.atan2( x, z ), 0 );
 			myPlayer.pitch = - Math.atan2( y, Math.hypot( x, z ) ) % 1.5;
-
 		}
-
 	}
-
 }
+
+
+const handleMouse = (e)=>
+    rightMouseDown = e.type === 'pointerdown';
+
+window.addEventListener( 'pointerdown', handleMouse );
+window.addEventListener( 'pointerup', handleMouse );
