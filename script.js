d89ca8559f03262835b34b048b988202d1e4245f
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2023-12-27 13:11:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -115,6 +115,117 @@ window[disguiseFuncName] = function(game){
 window.addEventListener("DOMContentLoaded", ()=>{
     window.onmousemove = ()=>{
         if (!vueApp?.game.on) 
-        PlayerNameInput.methods.onNameChange({target:{value:(Math.random()+1).toString(36).substring(2)}})
+            PlayerNameInput.methods.onNameChange({target:{value:(Math.random()+1).toString(36).substring(2)}})
     }
 })
+
+window[onUpdateFuncName] = function(BABYLON, players, myPlayer) {
+    if (!myPlayer) return;
+    
+    if (!lineOrigin){
+        lineOrigin = new BABYLON.Vector3();
+        linesArray = [];
+    }
+    
+    lineOrigin.copyFrom(myPlayer.actor.mesh.position);
+    
+    const yaw = myPlayer.actor.mesh.rotation.y;
+    
+    lineOrigin.x += Math.sin(yaw);
+    lineOrigin.z += Math.cos(yaw);
+    lineOrigin.y += Math.sin(-myPlayer.pitch);
+
+	for (let i=0; i<linesArray.length; i ++ )
+		linesArray[ i ].playerExists = false;
+
+	for ( let i = 0; i < players.length; i ++ ) {
+		const player = players[ i ];
+
+		if ( ! player || player === myPlayer ) continue;
+
+		if ( player.sphere === undefined ) {
+			console.log( 'Adding sphere...' );
+
+			const material = new BABYLON.StandardMaterial( 'myMaterial', player.actor.scene );
+			material.emissiveColor = material.diffuseColor = new BABYLON.Color3( 1, 0, 0 );
+			material.wireframe = true;
+
+			const sphere = BABYLON.MeshBuilder.CreateBox( 'mySphere', { width: 0.5, height: 0.75, depth: 0.5 }, player.actor.scene );
+			sphere.material = material;
+			sphere.position.y = 0.3;
+
+			sphere.parent = player.actor.mesh;
+
+			player.sphere = sphere;
+		}
+
+		if ( player.lines === undefined ) {
+			const options = {
+				points: [ lineOrigin, player.actor.mesh.position ],
+				updatable: true
+			};
+
+			const lines = options.instance = BABYLON.MeshBuilder.CreateLines( 'lines', options, player.actor.scene );
+			lines.color = new BABYLON.Color3( 1, 0, 0 );
+			lines.alwaysSelectAsActiveMesh = true;
+			lines.renderingGroupId = 1;
+
+			player.lines = lines;
+			player.lineOptions = options;
+
+			linesArray.push( lines );
+
+			console.log( '%cAdding line...', 'color: green; background: black; font-size: 2em;' );
+		}
+
+		player.lines.playerExists = true;
+		player.lines = BABYLON.MeshBuilder.CreateLines( 'lines', player.lineOptions );
+
+		player.sphere.renderingGroupId = 1
+		player.sphere.visibility = myPlayer !== player && ( myPlayer.team === 0 || myPlayer.team !== player.team );
+
+		player.lines.visibility = player.playing && player.sphere.visibility;
+
+	}
+
+	for ( let i = 0; i < linesArray.length; i ++ ) {
+		if ( ! linesArray[ i ].playerExists ) {
+			console.log( '%cRemoving line...', 'color: red; background: black; font-size: 2em;' );
+			linesArray[ i ].dispose();
+			linesArray.splice( i, 1 );
+		}
+	}
+
+	if (rightMouseDown && myPlayer.playing ) {
+		let minDistance = Infinity;
+		let targetPlayer;
+
+		for ( let i = 0; i < players.length; i ++ ) {
+			const player = players[ i ];
+
+			if ( player && player !== myPlayer && player.playing && ( myPlayer.team === 0 || player.team !== myPlayer.team ) ) {
+				const distance = Math.hypot( player.x - myPlayer.x, player.y - myPlayer.y, player.z - myPlayer.z );
+				if ( distance < minDistance ) {
+					minDistance = distance;
+					targetPlayer = player;
+				}
+			}
+		}
+
+		if ( targetPlayer ) {
+			const x = targetPlayer.actor.mesh.position.x - myPlayer.actor.mesh.position.x;
+			const y = targetPlayer.actor.mesh.position.y - myPlayer.actor.mesh.position.y;
+			const z = targetPlayer.actor.mesh.position.z - myPlayer.actor.mesh.position.z;
+
+			myPlayer.yaw = Math.radAdd( Math.atan2( x, z ), 0 );
+			myPlayer.pitch = - Math.atan2( y, Math.hypot( x, z ) ) % 1.5;
+		}
+	}
+}
+
+
+const handleMouse = (e)=>
+    rightMouseDown = e.type === 'pointerdown'
+
+window.addEventListener('pointerdown', handleMouse)
+window.addEventListener('pointerup', handleMouse)
